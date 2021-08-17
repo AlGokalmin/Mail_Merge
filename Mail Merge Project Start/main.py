d5c3c2cd4639e819b460777d0e57c49deb99c266
mihai <mihai.rachitan@gmail.com>
2021-08-17 21:54:45 +0300

    Initial commit

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,14 @@
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Names/invited_names.txt") as invited:
+    for name in invited.readlines():
+        with open(f"./Output/ReadyToSend/letter_to_{name}.txt", mode="w") as letter:
+            letter.write(f"Dear {name.strip()}, \n\nYou are invited to my birthday this Saturday.\n\nHope you can make "
+                         f"it!\n\nAngela")
